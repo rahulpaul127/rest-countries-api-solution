6412bbcb16e8dd5c602693a2f9fd50b3774c43a5
Rahul Paul <rahulpaul127001@gmail.com>
2026-05-29 13:09:53 +0530

    Configure Vercel deployment and Single Page App routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import DetailPage from './pages/DetailPage';
